06e2825f3d2b94eda70e07671db208d99e1aa959
kevansachs <kevansachs@users.noreply.github.com>
2026-04-04 22:08:18 -0400

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Quicksand, Poppins } from "next/font/google";
+import { Quicksand, Poppins, Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const quicksand = Quicksand({
   subsets: ["latin"],
@@ -34,7 +37,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={cn("font-sans", geist.variable)}>
       <body
         className={`${quicksand.variable} ${poppins.variable} font-body antialiased`}
       >
